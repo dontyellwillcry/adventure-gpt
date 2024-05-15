79a3c4e07a0c6d4186313e9430b91f5a5f4bbba6
dontyellwillcry <papaporo@proton.me>
2024-05-15 17:57:54 -0500

    created a utils folder, api folder, installed axios, pg and npm. created route.ts with temp DB request

---
+++ app/api/route.ts
@@ -0,0 +1,20 @@
+import pool from "@/utils/DB";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const result = await pool.query(`SELECT * FROM "ingredients";`);
+    if (result.status < 200 || result.status >= 300) {
+      throw new Error("Network response was not ok");
+    }
+    return NextResponse.json({ data: result.rows });
+    
+  } catch (error) {
+    console.error("Error with query", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+
+
