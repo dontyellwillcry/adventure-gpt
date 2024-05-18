bbde270deec60c8c5233400f63ec5de1dab4bc83
dontyellwillcry <papaporo@proton.me>
2024-05-18 03:19:24 -0500

    when selecting the genre, the background image will change to match the theme. Added a background component to hold the genres and the url paths to the image. imported into the hpme page. created a function that takes the current genre selected and sets the background to match

---
+++ app/components/backgrounds/Backgrounds.tsx
@@ -0,0 +1,26 @@
+interface Background {
+    genre: string;
+    imageUrl: string;
+  }
+  
+  
+  const Backgrounds: Background[] = [
+    {
+      genre: "Sci-fi",
+      imageUrl: "/images/Sci-fi.png",
+    },
+    {
+      genre: "Horror",
+      imageUrl: "/images/Fantasy3.jpg",
+    },
+    {
+      genre: "Fantasy",
+      imageUrl: "/images/Horror2.jpg",
+    },
+    {
+      genre: "Noir",
+      imageUrl: "/images/Cthulu.gif",
+    },
+  ];
+
+  export default Backgrounds;
--- app/components/sysContent/SystemContent.ts
+++ app/components/sysContent/SystemContent.ts
@@ -3,25 +3,27 @@ interface SystemContentProps {
   genre: string;
 }
 
-const SystemContent:SystemContentProps[] = [
+const SystemContent: SystemContentProps[] = [
   {
-    content: "You are a story teller crafting a Schi-fi experience for the user",
+    content:
+      "You are a story teller crafting a Schi-fi experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
     genre: "Sci-fi",
   },
   {
-    content: "You are a story teller crafting a Horror experience for the user",
+    content:
+      "You are a story teller crafting a Horror experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
     genre: "Horror",
   },
   {
-    content: "You are a story teller crafting a Fantasy experience for the user",
+    content:
+      "You are a story teller crafting a Fantasy experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
     genre: "Fantasy",
   },
   {
-    content: "You are a story teller crafting a Noir experience for the user",
+    content:
+      "You are a story teller crafting a Noir experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
     genre: "Noir",
-  }
+  },
 ];
-
-
 
 export default SystemContent;
