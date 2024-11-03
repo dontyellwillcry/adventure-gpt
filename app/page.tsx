ce133efda065de2f83038e9dfb06bdc61c7347ad
dontyellwillcry <papaporo@proton.me>
2024-11-02 21:46:41 -0500

    added a lib folder to move some items from the components folder that are not actually compoenents

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Backgrounds from "./components/backgrounds/Backgrounds";
 import ActionButtons from "./components/actions/ActionButtons";
 import FetchItems from "./components/fetchItems/FetchItems"
 // import Payload, { PayloadProps } from "./components/payload/Payload"
+import Payload, { PayloadProps } from "./components/payload/Payload" //test import
 const genres = ["Sci-fi", "Horror", "Fantasy", "Noir"];
 
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
   const [response, setResponse] = useState<string>(""); // The response from openai
   const [content, setContent] = useState<string>(""); // When the user selects the genre, a content query is saved for the api request
   const [currentGenre, setCurrentGenre] = useState<string>(""); // When the Genre is selected its added to this state to trigger the background
-  // const [payload, setPayload] - useState<PayloadProps>("");
+  const [payload, setPayload] = useState<PayloadProps>("");
 
 
   useEffect(() => {
