8b5f910c8a9811b914e0e560d97cfa5840a8e99b
dontyellwillcry <papaporo@proton.me>
2024-11-03 18:06:20 -0600

    debugging comments inside of route.js

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -4,8 +4,8 @@ import openai from "@/utils/openai";
 export async function POST(req) {
 
   const body = await req.json()
-  console.log(body.content)//dubugging purposes
-  console.log(body.playerActions)//dubugging purposes
+  console.log("This is the body.content inside of the openai route.js", body.content)//dubugging purposes
+  console.log("This is the body.playerActions inside of the openai route.js", body.playerActions)//dubugging purposes
   try {
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
