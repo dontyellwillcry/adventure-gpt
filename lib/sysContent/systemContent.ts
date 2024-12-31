bf86de2c3342f90482d1e9f2309c30b41954abf0
dontyellwillcry <papaporo@proton.me>
2024-12-31 16:52:59 -0600

    removed some commented code (old SystemConten info)

--- lib/sysContent/systemContent.ts
+++ lib/sysContent/systemContent.ts
@@ -1,7 +1,3 @@
-// interface SystemContentProps {
-//   content: string;
-//   genre: string;
-// }
 interface SystemContentProps {
   content?: string;
   genre?: string;
@@ -32,29 +28,6 @@ interface SystemContentProps {
 }
 
 // This needs to be moved to the DB once the table is created.
-// const SystemContent: SystemContentProps[] = [
-//   {
-//     content:
-//       "You are a story teller crafting a Schi-fi experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
-//     genre: "Sci-fi",
-
-//   },
-//   {
-//     content:
-//       "You are a story teller crafting a Horror experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
-//     genre: "Horror",
-//   },
-//   {
-//     content:
-//       "You are a story teller crafting a Fantasy experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
-//     genre: "Fantasy",
-//   },
-//   {
-//     content:
-//       "You are a story teller crafting a Noir experience for the user in a choose your own adventure format. Provide options for the user each time you craft the story",
-//     genre: "Noir",
-//   },
-// ];
 
 const SystemContent: SystemContentProps[] = [
   {
