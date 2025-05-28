bd4f240d777740c6a3461c0c1bf1a19c94816672
dontyellwillcry <papaporo@proton.me>
2025-05-27 20:31:47 -0500

    Forgot to change the name of the back end code from fetchItems to fetchUsers

--- app/components/fetchUsers/FetchUsers.ts
+++ app/components/fetchUsers/FetchUsers.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export default async function fetchItems() {
+export default async function fetchUsers() {
     try {
-      const response = await axios.get("/api/items")
+      const response = await axios.get("/api/users")
       console.log("This is my item", response.data)
     } catch (error) {
       console.error("There was a problem fetching from items table", error)
