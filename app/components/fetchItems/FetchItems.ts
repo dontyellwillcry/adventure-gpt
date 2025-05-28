33cecde98eef96572de7ea554633a80e31111e0b
dontyellwillcry <papaporo@proton.me>
2025-05-27 21:15:59 -0500

    added a return in for fetch requests for users and items.

--- app/components/fetchItems/FetchItems.ts
+++ app/components/fetchItems/FetchItems.ts
@@ -4,6 +4,7 @@ export default async function fetchItems() {
     try {
       const response = await axios.get("/api/items")
       console.log("This is my item", response.data)
+      return response.data
     } catch (error) {
       console.error("There was a problem fetching from items table", error)
     }
