4d008695b697fa9212fdd00b4a5381d409c60853
dontyellwillcry <papaporo@proton.me>
2025-06-08 01:11:39 -0500

    formatted ActionButtons

--- app/components/actions/ActionButtons.tsx
+++ app/components/actions/ActionButtons.tsx
@@ -3,20 +3,20 @@ import React, { useEffect } from "react";
 const ActionButtons: React.FC = () => {
   /* Is event.altKey true? Then check what key is pressed. In case 1-4 trigger something */
   const handleKeyPress = (event: KeyboardEvent) => {
-      switch (event.key) {
-        case "1":
-          console.log("You pressed 1");
-          break;
-        case "2":
-          console.log("You pressed 2");
-          break;
-        case "3":
-          console.log("You pressed 3");
-          break;
-        case "4":
-          console.log("You pressed 4");
-          break;
-      }
+    switch (event.key) {
+      case "1":
+        console.log("You pressed 1");
+        break;
+      case "2":
+        console.log("You pressed 2");
+        break;
+      case "3":
+        console.log("You pressed 3");
+        break;
+      case "4":
+        console.log("You pressed 4");
+        break;
+    }
   };
   /* This calls an event listener on the document and listens for a keydown event and also triggers the handleKeyPress function */
   useEffect(() => {
