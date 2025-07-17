4319fb8aa83ed6ee46d0fa519e8bd66809780f3e
dontyellwillcry <papaporo@proton.me>
2025-07-16 21:04:18 -0500

    changed the console.log in the FetchUsers.ts to say users instead of items

--- app/components/fetchUsers/FetchUsers.ts
+++ app/components/fetchUsers/FetchUsers.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function fetchUsers() {
   try {
     const response = await axios.get("/api/users");
-    console.log("This is my item", response.data);
+    console.log("These are our users", response.data);
     return response.data;
   } catch (error) {
     console.error("There was a problem fetching from users table", error);
