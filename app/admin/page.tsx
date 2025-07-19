c6e1f0940cc9a532fda762afc40f08baf86d3274
dontyellwillcry <papaporo@proton.me>
2025-07-19 17:45:20 -0500

    added nav bar to admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -10,18 +10,22 @@ export default function AdminPage() {
   }
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1>Admin Page</h1>
-      <div className="flex flex-col items-center justify-center w-1/2 h-[1000px] border-2 border-black rounded-2xl">
-        <form className="w-full flex flex-col items-center">
-          <input placeholder="Search" className="mb-4 p-2 border rounded" />
-          <button
+    <main className="flex flex-col items-center justify-start min-h-screen bg-white">
+      <nav className="w-full bg-gray-100 p-4 flex justify-between items-center">
+        <button
             type="button"
             onClick={handleRoute}
             className="flex items-center justify-center p-4 text-lg bg-blue-500 text-white rounded w-[150px] h-[42px] mt-4 "
           >
             Home
           </button>
+        navigation goes here
+      </nav>
+      <h1>Admin Page</h1>
+      <div className="flex flex-col items-center justify-center w-1/2 h-[1000px] border-2 border-black rounded-2xl">
+        <form className="w-full flex flex-col items-center">
+          <input placeholder="Search" className="mb-4 p-2 border rounded" />
+          
         </form>
       </div>
     </main>
