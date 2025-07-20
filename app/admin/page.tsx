563cb1d51be56497ee7027c9ae40e828c4fc9139
dontyellwillcry <papaporo@proton.me>
2025-07-19 22:54:33 -0500

    removed the useRouter and replaced it with Link for navigation in the nav bar

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import FetchUsers from "@/app/components/fetchUsers/FetchUsers";
+import Link from "next/link";
 
 type User = {
   id: number;
@@ -14,10 +15,6 @@ type User = {
 export default function AdminPage() {
   const [users, setUsers] = useState<User[]>([]);
 
-  const router = useRouter();
-  function handleRoute() {
-    router.push("/");
-  }
 
   useEffect(() => {
     async function getUsers() {
@@ -29,22 +26,16 @@ export default function AdminPage() {
 
   return (
     <main className="flex flex-col items-center justify-start min-h-screen bg-white">
-      <nav className="w-full bg-gray-100 p-4 flex justify-between items-center">
-        <button
-          type="button"
-          onClick={handleRoute}
-          className="flex items-center justify-center p-4 text-lg bg-blue-500 text-white rounded w-[150px] h-[42px] mt-4 "
-        >
-          Home
-        </button>
+      <nav className="w-full bg-gray-100 p-4 flex justify-between items-center h-[50px]">
+        <Link href="/">Home</Link>
         navigation goes here
       </nav>
       <h1>Admin Page</h1>
-      <div className="flex flex-col items-center justify-center w-1/2 h-[1000px] border-2 border-black rounded-2xl">
+      <div className="flex flex-col items-center justify-center w-1/2 h-[700px] border-2 border-black rounded-2xl">
         <form className="w-full flex flex-col items-center">
           <input placeholder="Search" className="mb-4 p-2 border rounded" />
         </form>
-        <table className="w-full mt-8 border">
+        <table className="w-full mt-8 border m-1">
           <thead>
             <tr>
               <th className="text-left p-2">Username</th>
